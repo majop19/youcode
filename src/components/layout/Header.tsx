660fd988d66909861f418165cc602f0d3350c3bc
majop19 <jp10ml2000@gmail.com>
2025-02-12 10:22:09 +0100

    add base app

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,25 @@
+import { SiteConfig } from "@/lib/site-config";
+import Link from "next/link";
+import { ThemeToggle } from "../theme/ThemeToggle";
+import { Typography } from "../ui/typography";
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container m-2 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <div className="flex items-center gap-2">
+          <Image src="/images/logo.svg" width={50} height={35} alt="app logo" />
+          <Typography variant="h3" as={Link} href="/">
+            {SiteConfig.title}
+          </Typography>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
